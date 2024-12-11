e390b0c7049d53ea77630d7960be0ca7613596a8
xkball <45330674+xkball@users.noreply.github.com>
2024-12-11 17:11:01 +0800

    Add: I18n texts. Bump Version to 1.4.42
    Took 52 minutes

--- src/main/java/org/teacon/powertool/block/DistantHorizonCheatingBlock.java
+++ src/main/java/org/teacon/powertool/block/DistantHorizonCheatingBlock.java
@@ -2,11 +2,16 @@ package org.teacon.powertool.block;
 
 import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 
 import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.List;
 
 @MethodsReturnNonnullByDefault
 @ParametersAreNonnullByDefault
@@ -21,5 +26,9 @@ public class DistantHorizonCheatingBlock extends Block implements ICosmeticBlock
         return 1f;
     }
     
-    
+    @Override
+    public void appendHoverText(ItemStack stack, Item.TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+        super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+        tooltipComponents.add(Component.translatable("tooltip.powertool.distant_horizon_cheating_block"));
+    }
 }
